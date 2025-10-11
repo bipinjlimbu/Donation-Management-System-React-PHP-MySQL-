9cdf806d375cd91c533562d26686dc63af8829eb
bipinjlimbu <bipinlimbu712@gmail.com>
2025-10-11 18:20:00 +0545

    Footer Added

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Footer() {
+    return (
+        <footer>
+            <p> &copy; 2024 Donation Management System. All rights reserved. </p>
+        </footer>
+    )
+}
