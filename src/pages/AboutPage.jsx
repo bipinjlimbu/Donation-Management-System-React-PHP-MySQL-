059f242b4a4abce28fed389cc66fb8d9a896f518
bipinjlimbu <bipinlimbu712@gmail.com>
2025-10-14 17:05:45 +0545

    About Page CSS Done

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,9 +1,9 @@
-import Footer from "../layout/Footer";
+import myAbout from '../style/AboutPage.module.css';
 
 
 export default function AboutPage() {
     return (
-        <div>
+        <div className={myAbout.about}>
             <h1>About Us</h1>
             <p>Welcome to our Donation Management System. We are dedicated to making a positive impact in our community by facilitating donations and connecting donors with those in need.</p>
             <p>Our mission is to streamline the donation process, ensuring that contributions reach the right people efficiently and effectively. We believe in transparency, accountability, and the power of collective action to create meaningful change.</p>
