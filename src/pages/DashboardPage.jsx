109d2ffded1a4b2e4a600e4528810f6a8a05de99
bipinjlimbu <bipinlimbu712@gmail.com>
2025-11-01 16:23:51 +0545

    Dashboard Updated

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -5,43 +5,58 @@ import axios from "axios"
 export default function DashboardPage() {
     const { user } = useAuth();
     const [profile, setProfile] = useState(null);
+    const [request, setRequest] = useState(null);
     const [error, setError] = useState(null);
     
     useEffect(() => {
-        if (user && user.email) {
-            axios
-                .get(`http://localhost/dms/api/profile.php?email=${user.email}`)
-                .then(res => {
-                    if (res.data.success) {
-                        setProfile(res.data.profile);
-                    } else {
-                        setError(res.data.message || "Failed to load user profile");
-                    }
-                })
-                .catch(err => {
-                    console.error("Failed to fetch user profile:", err);
-                    setError("Failed to connect to the server.");
-                });
+        if (user && user.user_email) {
+            const email = user.user_email;
+            axios.get(`http://localhost/dms/api/profile.php?email=${email}`)
+            .then(res => {
+                if (res.data.success) {
+                    setProfile(res.data.profile);
+                } else {
+                    setError(res.data.message || "Failed to load user profile");
+                }
+            })
+            .catch(err => {
+                console.error("Failed to fetch user profile:", err);
+                setError("Failed to connect to the server.");
+            });
         }
     }, [user]);
-    console.log("PROFILE DATA:", profile);
 
+    useEffect(()=>{
+        axios.get(`http://localhost/dms/api/donationPending.php`)
+            .then(res => {
+                if (res.data.success) {
+                    setRequest(res.data.request);
+                } else {
+                    setError(res.data.message || "No Pending Request");
+                }
+            })
+            .catch(err => {
+                console.error("Failed to fetch user profile:", err);
+                setError("Failed to connect to the server.");
+            });
+    },[]);
 
     return (
         <div>
-            {profile?.role === "Admin" ? (
+            {profile?.user_role === "Admin" ? (
                 <div>
                     <h1> Admin Dashboard </h1>
-                    <p> Welcome, {profile?.name}! Here you can manage campaigns and view donation statistics. </p>
+                    <p> Welcome, {profile?.user_name}! Here you can manage campaigns and view donation statistics. </p>
                 </div>
-            ) : profile?.role === "Donor" ? (
+            ) : profile?.user_role === "Donor" ? (
                 <div>
-                    <h1> Donor Dashboard </h1>
-                    <p> Welcome, {profile?.name}! Here you can view your donation history and manage your profile. </p>
+                    <h1> {profile.user_name} Dashboard </h1>
+
                 </div>
-            ) : profile?.role == "NGO" ? (
+            ) : profile?.user_role == "NGO" ? (
                 <div>
                     <h1>Donation Request</h1>
+                    <p> {request} </p>
                 </div>
             ):(
                 <div>
