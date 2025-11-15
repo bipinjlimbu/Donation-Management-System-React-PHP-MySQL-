a0513f7ab1a651d2c061e176e2d70357a017bb5a
bipinjlimbu <bipinlimbu712@gmail.com>
2025-11-15 16:02:25 +0545

    Profile Page Updated

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,37 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useAuth } from "../components/AuthContext";
 import { useNavigate } from "react-router-dom";
 import myProfile from "../style/ProfilePage.module.css";
-import axios from "axios";
 
 export default function ProfilePage() {
   const { user, logout } = useAuth();
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
-  const [profile, setProfile] = useState(null);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    if (!user) return;
-    axios.get("http://localhost/dms/api/profile.php", {
-      params: { user_id: user.user_id },
-    })
-      .then((response) => {
-        if (response.data.success) {
-          setProfile(response.data.profile);
-          setError("");
-        } else {
-          setError(response.data.message || "Failed to fetch profile.");
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-        setError("Network or server error");
-      });
-  }, [user]);
-
-  console.log(profile);
-  
 
   const handleLogout = async () => {
     setLoading(true);
@@ -44,31 +19,19 @@ export default function ProfilePage() {
     return <p>Loading user...</p>;
   }
 
-  if (!profile && !error) {
-    return <p>Loading profile...</p>;
-  }
-
   return (
     <div className={myProfile.container}>
       <h1>My Profile</h1>
 
-      {error ? (
-        <p>{error}</p>
-      ) : (
-        <>
-          <p><strong>Name:</strong> {profile?.username}</p>
-          <p><strong>Role:</strong> {profile?.role}</p>
-          <p><strong>Email:</strong> {profile?.email}</p>
-        </>
-      )}
+      <p><strong>Name:</strong> {user.username}</p>
+      <p><strong>Role:</strong> {user.role}</p>
+      <p><strong>Email:</strong> {user.email}</p>
+
       <div className={myProfile.buttons}>
         <button onClick={() => navigate("/editProfile")}>
           Edit Profile
         </button>
-        <button
-          onClick={handleLogout}
-          disabled={loading}
-        >
+        <button onClick={handleLogout} disabled={loading}>
           {loading ? "Logging out..." : "Logout"}
         </button>
       </div>
