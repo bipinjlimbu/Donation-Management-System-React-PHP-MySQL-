744c213b5c35f54bbdde73a96bdfee03a6395a3f
bipinjlimbu <bipinlimbu712@gmail.com>
2025-11-16 16:08:53 +0545

    Navbar Updated

--- src/layout/navbar.jsx
+++ src/layout/navbar.jsx
@@ -11,6 +11,7 @@ export default function () {
                 <Link to = "/"> Home </Link>
                 <Link to = "/dashboard"> Dashboard </Link>
                 <Link to = "/records"> Records </Link>
+                <Link to = "/notifications"> Notifications </Link>
                 <Link to = "/campaigns"> Campaigns </Link>
                 <Link to = "/contact"> Contact </Link>
                 <Link to = "/about"> About </Link>
