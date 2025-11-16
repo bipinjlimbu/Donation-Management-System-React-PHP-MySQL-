55ed88f796c188bb45e5c37de7099d136a33e0c8
bipinjlimbu <bipinlimbu712@gmail.com>
2025-11-16 16:03:49 +0545

    Connect DB Backend Updated

--- backend/dms/api/connectDB.php
+++ backend/dms/api/connectDB.php
@@ -1,19 +1,21 @@
 <?php
-	class connectDB {
-		private $server = 'localhost';
-		private $dbname = 'dms';
-		private $user = 'root';
-		private $pass = '';
+class connectDB
+{
+	private $server = 'localhost';
+	private $dbname = 'dms';
+	private $user = 'root';
+	private $pass = '';
 
-		public function connect() {
-			try {
-				$conn = new PDO('mysql:host=' .$this->server .';dbname=' . $this->dbname, $this->user, $this->pass);
-				$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-				return $conn;
-			} catch (\Exception $e) {
-				echo "Database Error: " . $e->getMessage();
-			}
+	public function connect()
+	{
+		try {
+			$conn = new PDO('mysql:host=' . $this->server . ';dbname=' . $this->dbname, $this->user, $this->pass);
+			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			return $conn;
+		} catch (\Exception $e) {
+			echo "Database Error: " . $e->getMessage();
 		}
-        
 	}
+
+}
 ?>
