7a1e6b0f62687d53bbcd608f75f7f91d7ab354a5
bipinjlimbu <bipinlimbu712@gmail.com>
2025-11-17 17:10:28 +0545

    Notifications Page Updated

--- src/pages/NotificationsPage.jsx
+++ src/pages/NotificationsPage.jsx
@@ -1,9 +1,12 @@
-
+import { userState, useEffect} from "react"
+import { useAuth } from "../components/AuthContext"
+import axios from "axios"
 
 export default function NotificationsPage() {
     return(
         <>
             <h1>Notifications Page</h1>
+            
         </>
     )
 }
