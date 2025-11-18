9601b3ccd23923920dc5110e23957008360b0efc
bipinjlimbu <bipinlimbu712@gmail.com>
2025-11-18 17:31:11 +0545

    Campaign Page Backend Updated

--- backend/dms/api/campaigns.php
+++ backend/dms/api/campaigns.php
@@ -13,15 +13,22 @@ $method = $_SERVER['REQUEST_METHOD'];
 switch ($method) {
     case 'GET':
         try {
-            $conn->exec("UPDATE campaigndetails SET status='Completed' WHERE status='Active' AND end_date < CURDATE() OR target_quantity <= collected_quantity");
+            $update = $conn->prepare("UPDATE Campaigns SET status = 'Completed' WHERE status = 'Active'
+                AND (end_date < CURDATE() OR target_quantity <= collected_quantity)");
+            $update->execute();
 
-            $stmt = $conn->prepare("SELECT * FROM campaigndetails");
+            $stmt = $conn->prepare("
+                SELECT campaign_id, ngo_id, title, description, item_name,target_quantity, collected_quantity, unit, status,start_date, end_date, requested_at, approved_at
+                FROM Campaigns ORDER BY start_date DESC
+            ");
             $stmt->execute();
             $campaigns = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
             echo json_encode([
                 "success" => true,
                 "campaigns" => $campaigns
             ]);
+
         } catch (PDOException $e) {
             http_response_code(500);
             echo json_encode([
@@ -30,6 +37,7 @@ switch ($method) {
             ]);
         }
         break;
+
     default:
         http_response_code(405);
         echo json_encode([
