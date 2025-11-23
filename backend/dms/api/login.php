3d57fbc225f4a45274b8e6e5df09fae58c77b1cd
bipinjlimbu <bipinlimbu712@gmail.com>
2025-11-23 18:05:35 +0545

    Login Page Backend Updated

--- backend/dms/api/login.php
+++ backend/dms/api/login.php
@@ -49,7 +49,6 @@ echo json_encode([
     "user" => [
         "user_id" => $user['user_id'],
         "email" => $user['email'],
-        "username" => $user['username'],
         "role" => $user['role']
     ]
 ]);
