6a2ad3ab8f3550b65883a85866a92504407895da
bipinjlimbu <bipinlimbu712@gmail.com>
2025-12-06 19:54:44 +0545

    Sign Up Page Backend Updated

--- backend/dms/api/signup.php
+++ backend/dms/api/signup.php
@@ -11,36 +11,38 @@ $conn = $objDb->connect();
 $email = strtolower(trim($_POST['email'] ?? ''));
 $password = trim($_POST['password'] ?? '');
 $role = $_POST['role'] ?? 'Donor';
+$name = trim($_POST['name'] ?? '');
+$phone = trim($_POST['phone'] ?? '');
+$address = trim($_POST['address'] ?? '');
 $registration_number = trim($_POST['registration_number'] ?? null);
 $requested_at = date('Y-m-d H:i:s');
 
-if (!$email || !$password) {
+if (!$email || !$password || !$name) {
     echo json_encode(["success" => false, "message" => "Required fields missing"]);
     exit;
 }
 
-$sql = "SELECT email FROM users WHERE email = :email";
+$sql = "SELECT r.register_id 
+        FROM register r 
+        WHERE r.status = 'Approved' AND r.user_id IN (SELECT user_id FROM users WHERE email = :email)";
 $stmt = $conn->prepare($sql);
 $stmt->bindParam(':email', $email);
 $stmt->execute();
 if ($stmt->fetch()) {
-    echo json_encode(["success" => false, "message" => "Email already exists"]);
+    echo json_encode(["success" => false, "message" => "Email already registered and approved"]);
     exit;
 }
 
 $verificationFilePath = null;
-
 if ($role === "NGO") {
-
     if (!isset($_FILES["verification_file"])) {
         echo json_encode(["success" => false, "message" => "Verification file required"]);
         exit;
     }
 
     $uploadDir = "uploads/verification/";
-    if (!file_exists($uploadDir)) {
+    if (!file_exists($uploadDir))
         mkdir($uploadDir, 0777, true);
-    }
 
     $filename = time() . "_" . basename($_FILES["verification_file"]["name"]);
     $targetFile = $uploadDir . $filename;
@@ -55,13 +57,15 @@ if ($role === "NGO") {
 
 $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
 
-$sql = "INSERT INTO users (email, password_hash, role, registration_number, verification_file, status, requested_at)
-        VALUES (:email, :password_hash, :role, :registration_number, :verification_file, 'Pending', :requested_at)";
-
+$sql = "INSERT INTO register 
+        (user_id, role, name, phone, address, registration_number, verification_file, status, requested_at) 
+        VALUES 
+        (NULL, :role, :name, :phone, :address, :registration_number, :verification_file, 'Pending', :requested_at)";
 $stmt = $conn->prepare($sql);
-$stmt->bindParam(':email', $email);
-$stmt->bindParam(':password_hash', $hashedPassword);
 $stmt->bindParam(':role', $role);
+$stmt->bindParam(':name', $name);
+$stmt->bindParam(':phone', $phone);
+$stmt->bindParam(':address', $address);
 $stmt->bindParam(':registration_number', $registration_number);
 $stmt->bindParam(':verification_file', $verificationFilePath);
 $stmt->bindParam(':requested_at', $requested_at);
