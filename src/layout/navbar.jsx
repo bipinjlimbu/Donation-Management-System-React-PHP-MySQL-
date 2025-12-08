85b632d62bc7fa49404c174dff0e4653d84bf00a
bipinjlimbu <bipinlimbu712@gmail.com>
2025-12-08 17:53:09 +0545

    Navbar Updated

--- src/layout/navbar.jsx
+++ src/layout/navbar.jsx
@@ -1,25 +1,29 @@
-import {Link} from "react-router-dom"
-import { useAuth } from "../components/AuthContext"
+import { Link } from "react-router-dom";
+import { useAuth } from "../components/AuthContext";
 
-export default function () {
+export default function Navbar() {
     const { user } = useAuth();
 
-    return(
+    return (
         <div className="nav">
-            <Link to = "/"><h1> ShareHope </h1></Link>
+            <Link to="/"><h1>ShareHope</h1></Link>
             <nav>
-                <Link to = "/"> Home </Link>
-                <Link to = "/dashboard"> Dashboard </Link>
-                <Link to = "/records"> Records </Link>
-                <Link to = "/campaigns"> Campaigns </Link>
-                <Link to = "/contact"> Contact </Link>
-                <Link to = "/about"> About </Link>
-                
-                {user ?
-                    <Link to = "/profile"> <img src="src/images/profile.webp" alt="profile" className="profImg"/> </Link>
-                    :
-                    <Link to = "/login"><button className="LogBut"> Login </button></Link>
-                }
+                <Link to="/">Home</Link>
+                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/records">Records</Link>
+                <Link to="/campaigns">Campaigns</Link>
+                <Link to="/contact">Contact</Link>
+                <Link to="/about">About</Link>
+
+                {user ? (
+                    <Link to="/profile">
+                        <img src="src/images/profile.webp" alt="profile" className="profImg"/>
+                    </Link>
+                ) : (
+                    <Link to="/login">
+                        <button className="LogBut">Login</button>
+                    </Link>
+                )}
             </nav>
         </div>
     )
