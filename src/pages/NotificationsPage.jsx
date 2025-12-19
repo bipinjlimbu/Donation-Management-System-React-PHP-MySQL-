2c85a145fd2f5be5c789230b9388a6902a447dc1
bipinjlimbu <bipinlimbu712@gmail.com>
2025-12-19 17:56:40 +0545

    Notifications Page Updated

--- src/pages/NotificationsPage.jsx
+++ src/pages/NotificationsPage.jsx
@@ -1,9 +1,16 @@
-
+import { useAuth } from "../components/AuthContext";
+import LoginPage from "./LoginPage";
 
 export default function NotificationsPage() {
-    return (
-        <div>
-            <h1>Notifications Page - Coming Soon!</h1>
-        </div>
-    );
+    const { user } = useAuth();
+
+    if (!user) return <LoginPage />;
+
+    else{
+        return (
+            <div>
+                <h1>Notifications Page - Coming Soon!</h1>
+            </div>
+        );
+    }
 }
