d4e36d8818cc7beb312e995dd4f0a719e7f309e5
bipinjlimbu <bipinlimbu712@gmail.com>
2026-02-08 21:45:12 +0545

    User List Page Added

---
+++ src/pages/UserListPage.jsx
@@ -0,0 +1,10 @@
+
+
+export default function UserListPage() {
+    return (
+        <div style={{ padding: "20px" }}>
+            <h1>User List</h1>
+            <p>This page is under construction. Only Admins can see this.</p>
+        </div>
+    );
+}
